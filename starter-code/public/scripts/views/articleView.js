961f8a22441d234d68cb558b1b7779260114a40c
nerdbetter <jasontlogan@gmail.com>
2017-07-24 14:47:57 -0500

    added adminView.js content to articleView.js

--- starter-code/public/scripts/views/articleView.js
+++ starter-code/public/scripts/views/articleView.js
@@ -88,6 +88,14 @@ var app = app || {};
     articleView.setTeasers();
     $('pre code').each((i, block) => hljs.highlightBlock(block));
   };
+  articleView.initAdminPage = () => {
+    let template = Handlebars.compile($('#author-template').text());
+    app.Article.numWordsByAuthor().forEach(stat => {
+      $('.author-stats').append(template(stat));
+    });
+    $('#blog-stats .articles').text(app.Article.all.length);
+    $('#blog-stats .words').text(app.Article.numWordsAll());
+  }
 
   //app.Article.fetchAll(articleView.initIndexPage);
   module.articleView = articleView;
