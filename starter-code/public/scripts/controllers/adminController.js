2ce1ae1e7f5e151864279640410c32c407fc8355
nerdbetter <jasontlogan@gmail.com>
2017-07-24 14:40:58 -0500

    added adminController

---
+++ starter-code/public/scripts/controllers/adminController.js
@@ -0,0 +1,15 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+  const adminController = {};
+
+  adminController.showAdmin = () => {
+    $('.tab-content').hide();
+    $('#admin').fadeIn();
+  }
+  // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
+
+
+  module.adminController = adminController;
+})(app);
